01a33cfe1d7bbd0d90e1cd6f4a5bdb53b5c692a6
Rafael Torres Souza <rafaatsouza@Rafaels-MacBook-Air-2.local>
2022-11-19 15:58:47 -0300

    adds new commit for the current semester

--- mining/const.py
+++ mining/const.py
@@ -4,6 +4,7 @@ TARGET_DATA = {
         { 'semester': '2019-01', 'hash_commit': '591ea76cdb44c79d4cd2d8d9e461ed2c3d38918b' },  # Jun 19, 2019
         { 'semester': '2020-01', 'hash_commit': 'c3c9afd85e71a53846016e31bf9e65f6cf15ecf9' },  # Oct 14, 2020
         { 'semester': '2020-02', 'hash_commit': '74fcb9ee883aac8e46510ae6af005934af215968' },  # Jan 22, 2021
-        { 'semester': '2022-01', 'hash_commit': '11e62864d01bde4af32a184c6bf3ece5e0a0dc51' }  # Feb 6, 2022
+        { 'semester': '2022-01', 'hash_commit': '11e62864d01bde4af32a184c6bf3ece5e0a0dc51' }, # Feb 6, 2022
+        { 'semester': '2022-02', 'hash_commit': '085ea49ed7db5478076ce5f4502d6f1620f42fc4'} 
     ]
 }
