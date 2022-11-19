7ea2beeeb43f5fbd8e7e30861b9dd3339afacf70
Rafael Torres Souza <rafaatsouza@Rafaels-MacBook-Air-2.local>
2022-11-19 15:37:53 -0300

    adds top by metric

--- analysis/data_methods.py
+++ analysis/data_methods.py
@@ -1,5 +1,6 @@
-import os
+import os, re
 import pandas as pd
+import numpy as np
 
 
 def get_dataframes():
@@ -13,3 +14,82 @@ def get_dataframes():
         for p in os.listdir('../data/')
     }
 
+
+def get_metrics_analysis(df, metrics_by_class, metrics_by_method):
+    def _get_data_without_tests(df):
+        reg = re.compile(r'.*test.*', re.IGNORECASE)    
+        return df[~df['file'].str.match(reg, na=False)]
+
+    analysis = {}
+    samples = [
+        { 
+            'key': 'class', 
+            'dfs': {
+                'default': df['classes'],
+                'not-test': _get_data_without_tests(df['classes']),
+            }, 
+            'metrics': metrics_by_class,
+            'get_metric_content': lambda record, metric: { 
+                'class': record['class'], 
+                'metric': record[metric],
+            }
+        },
+        { 
+            'key': 'method', 
+            'dfs': {
+                'default': df['methods'],
+                'not-test': _get_data_without_tests(df['methods']),
+            }, 
+            'metrics': metrics_by_method,
+            'get_metric_content': lambda record, metric: { 
+                'class': record['class'], 
+                'method': record['method'], 
+                'metric': record[metric],
+            }
+        },
+    ]
+
+    for s in samples:
+        key = s['key']
+        for df_name, df in s['dfs'].items():
+            for metric in s['metrics']:
+                avg_metric_classes = np.average(np.array(df[metric].values))
+                std_metric_classes = np.std(np.array(df[metric].values))
+                top_metric_classes = df[df[metric] > (avg_metric_classes + std_metric_classes)]
+                records = [ 
+                    s['get_metric_content'](r[1], metric) 
+                    for r in top_metric_classes.sort_values(metric, ascending=False).iterrows() 
+                ]
+
+                if metric not in analysis:
+                    analysis[metric] = {}
+                if key not in analysis[metric]:
+                    analysis[metric][key] = {}
+
+                analysis[metric][key][df_name] = {
+                    'avg': avg_metric_classes,
+                    'std': std_metric_classes,
+                    'records': records
+                }
+
+    return analysis
+
+
+def get_data_for_top_by_metric(analysis, target, top=10):
+    result = {}
+    if target not in ['class', 'method']:
+        return result
+
+    for metric, metric_content in analysis.items():
+        if target in metric_content:
+            result[metric] = {'avg': metric_content[target]['default']['avg'], 'stdev': metric_content[target]['default']['std']}
+
+            not_test_records = [(r[target], r['metric']) for r in metric_content[target]['default']['records']]
+            not_test_records = sorted(not_test_records, key=lambda x: x[1], reverse=True)
+
+            if len(not_test_records) > top:
+                not_test_records = not_test_records[:top]
+
+            result[metric]["records"] = not_test_records
+
+    return result
